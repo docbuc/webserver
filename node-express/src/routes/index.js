010343466bc3f7b70a5cd9340b9e3a7f99bc989d
Bernd Oeggl <bernd@webman.at>
2021-04-08 15:34:51 +0200

    switch to ejs templating engine
    no npm security problems for ejs

--- node-express/src/routes/index.js
+++ node-express/src/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+const debug = require('debug')('server');
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  debug('Kopfzeilen: ', req.headers)
   res.render('index', { title: 'Express' });
 });
 
