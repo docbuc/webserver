9959492200f7c5128ee3612eca35bc1d793f0116
Bernd Oeggl <bernd@webman.at>
2021-07-01 08:41:37 +0200

    update node-express

--- node-express/src/routes/index.js
+++ node-express/src/routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-const debug = require('debug')('server');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  debug('Kopfzeilen: ', req.headers)
   res.render('index', { title: 'Express' });
 });
 
